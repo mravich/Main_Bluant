b12e9e4e9bde3defc1e65e34542ff623d3f9fe7b
mravich <sanjinm994@gmail.com>
2017-10-03 12:29:53 -0700

    MenuSceneOptim
    Fixed screen size for MEnu scene

---
+++ Blu Rocket Final 3.0/Assets/PlayButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour {
+
+	public AnimationClip explode;
+
+	void Start()
+	{
+
+		Animator anim = GetComponent<Animator> ();
+	
+	}
+	void OnMouseOver(){
+
+	
+	}
+
+	void OnMouseExit(){
+	
+		foreach (Transform obj in transform) {
+
+			foreach (Transform obj2 in obj) {
+				
+
+
+			}
+		}
+	}
+
+	void OnMouseDown(){
+		
+
+		print ("Pokreni igru");
+	
+
+
+	}
+
+}
