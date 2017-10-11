837e9fcc2383127872882ed2078b212e52221a5b
mravich <sanjinm994@gmail.com>
2017-10-11 08:22:24 -0700

    Simple Web Browser
    Web browser forms

---
+++ C# .NET learning/BluantWebBrowser-simple/BluantWebBrowser-simple/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BluantWebBrowser_simple
+{
+    public partial class t : Form
+    {
+        public t()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is called when exit is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program is made by mravich");
+        }
+
+        private void t_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// On click of this button the web control will display the page requested
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NavigateToPage();
+        }
+        /// <summary>
+        /// This is the core function for navigaion
+        /// </summary>
+        private void NavigateToPage() {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            toolStripStatusLabel1.Text = "Navigation has started";
+            webBrowser1.Navigate(textBox1.Text);
+        }
+        /// <summary>
+        /// This function will fire when you push a key and release
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter) {
+                NavigateToPage();
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            toolStripStatusLabel1.Text = "Navigation has completed";
+
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if(e.CurrentProgress>0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+
+            }
+        }
+    }
+}
